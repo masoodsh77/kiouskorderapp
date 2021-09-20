56fbb1c22fdedbcb80e8087c9452610ed6f85e18
masoodsh77 <masoodsh77@gmail.com>
2021-09-20 13:26:46 +0430

    Charts bux fixed & beautify this

--- src/Pages/dashboard/DashHome/Components/Chart.js
+++ src/Pages/dashboard/DashHome/Components/Chart.js
@@ -3,6 +3,9 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const options = {
+  chart: {
+    type: "spline",
+  },
   title: {
     text: "",
   },
@@ -22,13 +25,14 @@ const options = {
       "اسفند",
     ],
   },
-  yAxis:{
-    title:""
+  yAxis: {
+    title: "",
   },
   series: [
     {
       name: "درآمد",
-      data: [1, 2, 3, 4, 5, 6, 7, 2, 1, 0, 1, 30],
+      data: [1, 2, 3, 4, 5, 6, 7, 2, 1, 0.5, 1, 15],
+      color: "#DE306F",
     },
   ],
 };
