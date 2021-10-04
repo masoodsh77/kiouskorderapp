dd985694e82e8b15e4854272e9de55dba37459e8
masoodsh77 <masoodsh77@gmail.com>
2021-10-04 08:09:53 +0330

    add dynamic resturants

--- src/Pages/Home/Resturants/Resturants.jsx
+++ src/Pages/Home/Resturants/Resturants.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Resturants.css";
-import Logo from '../../../assets/img/logo.png'
+import Logo from "../../../assets/img/logo.png";
+import axios from "axios";
 function Resturants() {
+  const [restu, setRestu] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:1337/categories").then((res) => {
+      console.log(res.data);
+      setRestu(res.data);
+    });
+  }, []);
   return (
     <div className="d-flex justify-content-center resturants flex-wrap">
       <div className="resturantsHeader w-100 text-center">
@@ -9,10 +17,14 @@ function Resturants() {
       </div>
       <div className="resturantsCards w-100">
         <div className="row p-2 s-flex justify-content-center">
-          <div className="resturantsCard col-3 d-flex justify-content-center align-items-center flex-wrap m-1">
-            <img src={Logo} alt="" width="95rem"/>
-            <div className="w-100 text-center">نام رستوران</div>
-          </div>
+          {restu.map((item, i) => {
+            return (
+              <div key={i} className="resturantsCard col-3 d-flex justify-content-center align-items-center flex-wrap m-1">
+                <img src={Logo} alt="" width="95rem" />
+                <div className="w-100 text-center">{item.name}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
